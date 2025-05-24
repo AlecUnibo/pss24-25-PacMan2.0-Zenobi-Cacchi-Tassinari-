7bdf497feee3b5111b9cd03ef449188ec17dfd32
AlecUnibo <alessandro.cacchi@studio.unibo.it>
2025-05-24 12:02:48 +0200

    corretto Block.java +  fix bug movimento

--- src/main/java/com/pacman/Block.java
+++ src/main/java/com/pacman/Block.java
@@ -1,4 +1,3 @@
-// File: Block.java
 package com.pacman;
 
 import javafx.scene.image.Image;
@@ -7,19 +6,18 @@ public class Block {
     public int x, y, width, height;
     public Image image;
     public final Image originalImage;
-
     public final GhostType ghostType;
-
+    public Direction direction;
     public boolean isExiting = false;
-
     public boolean isScared  = false;
-
     public int velocityX = 0, velocityY = 0;
 
+    // Costruttore per blocchi di dimensione di default (tile Pac-Man)
     public Block(Image image, int x, int y) {
         this(image, x, y, PacMan.TILE_SIZE, PacMan.TILE_SIZE, null);
     }
 
+    // Costruttore completo per blocchi (muri, cibo, fantasmi) con tipo GhostType opzionale
     public Block(Image image, int x, int y, int width, int height, GhostType ghostType) {
         this.image         = image;
         this.originalImage = image;
@@ -28,9 +26,11 @@ public class Block {
         this.width         = width;
         this.height        = height;
         this.ghostType     = ghostType;
+        this.direction = (ghostType != null) ? Direction.randomDirection() : null;
         this.isScared  = false;
     }
 
+    // Aggiorna la direzione di movimento e l’immagine in base al carattere di direzione (L/R/U/D)
     public void updateDirection(char dir, Image img) {
         this.image = img;
         this.velocityX = (dir == 'L' ? -4 : dir == 'R' ? 4 : 0);
